4a6ea56519dad402f41c9abb5332c4a997697037
Charlie W <charliew.@cw2015laptop.local>
2020-06-27 20:16:25 +0800

    done

---
+++ lib/views/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_futter01/views/auth/auth.dart';
+import 'package:firebase_futter01/views/index/index.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // go to home or auth view
+    return Auth();
+  }
+}
